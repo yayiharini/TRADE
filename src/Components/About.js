2497369e765b53bec0f0f0835ec0e159bba9b277
Yayi <hariniyayi@csus.edu>
2022-01-01 18:15:49 -0800

    Testing upstream link

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 function About() {
     return (
         <div>
-            <h2>THIS IS THE ABOUT PAGE</h2>
+            <h2>THIS IS THE ABOUT PAGE by harini</h2>
         </div>
     );
 }
